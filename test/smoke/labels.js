ff07a58ce6ecc4631264b7c7c7901422b5b1a4ba
Andrey Danilov <andreydanilov@Andreys-MacBook-Pro.local>
2020-11-17 19:18:47 -0500

    add data and helpers

--- test/smoke/labels.js
+++ test/smoke/labels.js
@@ -36,28 +36,38 @@ describe('My Little Hero', function () { //define suite title by passing a strin
             expect(label).toEqual(true);
         });
 
+        it('TC-006 Label for image', function () {
+            const label = $$(sel.image)[4].isDisplayed();
+            expect(label).toEqual(true);
+        });
+
     });
     describe('Labels are correct', function () {
 
-        it('TC-006 Label for name = 1. What is your Hero\'s name?', function () {
+        it('TC-007 Label for name = 1. What is your Hero\'s name?', function () {
             const text = $$(sel.label)[0].getText('title');
             expect(text).toEqual(exp.labelName);
         });
 
-        it('TC-007 Label for gender = 2. Please choose a gender.', function () {
+        it('TC-008 Label for gender = 2. Please choose a gender.', function () {
             const text = $$(sel.label)[1].getText('title');
             expect(text).toEqual(exp.labelGender);
         });
 
-        /*it('TC-008 Label for age', function () {
-            const label = $$('.ant-form-item-required')[2].isDisplayed();
-            expect(label).toEqual(true);
+        it('TC-009 Label for age = 3. How old is your hero?', function () {
+            const text = $$(sel.label)[2].getText('title');
+            expect(text).toEqual(exp.labelAge);
         });
 
-        it('TC-009 Label for story', function () {
-            const label = $$('.ant-form-item-required')[3].isDisplayed();
-            expect(label).toEqual(true);
-        });*/
+        it('TC-010 Label for story = 4. What type of story would you like to read?', function () {
+            const text = $$(sel.label)[3].getText('title');
+            expect(text).toEqual(exp.labelStory);
+        });
+
+        it('TC-011 Label for image = 5. Upload an image (optional)', function () {
+            const text = $$(sel.image)[4].getText('title');
+            expect(text).toEqual(exp.labelImage);
+        });
 
     });
 
